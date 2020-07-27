dabca0ac1977a86b10db4eb34c2bf46398887777
Divya <35322960+divyaskills@users.noreply.github.com>
2020-07-27 14:02:24 +0530

    Add files via upload

--- Models/VehicleModel.cs
+++ Models/VehicleModel.cs
@@ -6,12 +6,15 @@ namespace TestCode.Models
   {
     public long Id { get; set; }
     public string Name { get; set; }
-    public string JourneyOrigin { get; set; }
-    public string JourneyEndPoint { get; set; }
+    public string VehicleOrigin { get; set; }
+    public string VehicleEndPoint { get; set; }
     public string VehicleNumber { get; set; }
-    public string Source { get; set; }
-    public string Destination { get; set; }
+    public string PickupPoint { get; set; }
+    public string EndPoint { get; set; }
     public DateTime JourneyStart { get; set; }
     public DateTime JourneyEnd { get; set; }
+    public DateTime PickupPointArrival { get; set; }
+    public DateTime EndPointArrival { get; set; }
+    public int SeatAvailabilityCount { get; set; }
   }
 }
